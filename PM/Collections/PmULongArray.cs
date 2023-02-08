22ca6bb3118be8f85694fcc2cc764601f718d4ed
Guirelli <henrique.guirelli@intelitrader.com.br>
2023-02-07 21:21:34 -0300

    Create Ulong array

---
+++ PM/Collections/PmULongArray.cs
@@ -0,0 +1,20 @@
+﻿namespace PM.Collections
+{
+    internal class PmULongArray : PmPrimitiveArray<ulong>
+    {
+        public PmULongArray(string filepath, int capacity)
+            : base(filepath, capacity)
+        {
+        }
+
+        protected override void InternalSet(int index, ulong value)
+        {
+            _cSharpDefinedPm.WriteULong(value, sizeof(ulong) * index);
+        }
+
+        protected override ulong InternalGet(int index)
+        {
+            return _cSharpDefinedPm.ReadULong(sizeof(ulong) * index);
+        }
+    }
+}
