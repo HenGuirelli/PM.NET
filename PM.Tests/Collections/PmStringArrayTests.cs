eec5868bd6b70705ebfaa6622928db79555c3923
Guirelli <henrique.guirelli@intelitrader.com.br>
2023-02-07 21:58:46 -0300

    Create string array

---
+++ PM.Tests/Collections/PmStringArrayTests.cs
@@ -0,0 +1,40 @@
+﻿using PM.Collections;
+using PM.Configs;
+using PM.Tests.Common;
+using System;
+using Xunit;
+
+namespace PM.Tests.Collections
+{
+    public class PmStringArrayTests
+    {
+        public PmStringArrayTests()
+        {
+            if (Constraints.UseFakePm)
+                PmGlobalConfiguration.PmTarget = PmTargets.InVolatileMemory;
+        }
+
+        [Fact]
+        public void OnSetAndGet_ShouldRunWithoutException()
+        {
+            var array = new PmStringArray(nameof(OnSetAndGet_ShouldRunWithoutException), length: 2);
+
+            var val0 = Guid.NewGuid().ToString();
+            var val1 = Guid.NewGuid().ToString();
+            array[0] = val0;
+            array[1] = val1;
+
+            Assert.Equal(val0, array[0]);
+            Assert.Equal(val1, array[1]);
+        }
+
+        [Fact]
+        public void OnSetAndGetStringOutOfBounds_ShouldThrowException()
+        {
+            var array = new PmStringArray(nameof(OnSetAndGet_ShouldRunWithoutException), length: 1);
+
+            Assert.Throws<IndexOutOfRangeException>(() => array[1] = Guid.NewGuid().ToString());
+            Assert.Throws<IndexOutOfRangeException>(() => array[1]);
+        }
+    }
+}
---
+++ PM/Collections/PmStringArray.cs
@@ -0,0 +1,57 @@
+﻿using PM.Configs;
+using PM.Core;
+using PM.Factories;
+using PM.Managers;
+
+namespace PM.Collections
+{
+    public class PmStringArray
+    {
+        private readonly PmULongArray _pmULongArray;
+        private static readonly PointersToPersistentObjects _pointersToPersistentObjects = new();
+
+        public int Length { get; private set; }
+
+        public string this[int index]
+        {
+            get => Get(index);
+            set => Set(index, value);
+        }
+
+        public PmStringArray(string filepath, int length)
+        {
+            _pmULongArray = new PmULongArray(filepath, length);
+        }
+
+        private void Set(int index, string value)
+        {
+            if (index > Length) throw new IndexOutOfRangeException();
+
+            var pointer = _pointersToPersistentObjects.GetNext();
+            var pm = PmFactory.CreatePm(
+                new PmMemoryMappedFileConfig(
+                    Path.Combine(PmGlobalConfiguration.PmInternalsFolder, pointer.ToString()),
+                    sizeof(char) * (value.Length + 1)));
+
+            var cSharpDefinedPm = new PmCSharpDefinedTypes(pm);
+            cSharpDefinedPm.WriteString(value);
+
+            _pmULongArray[index] = pointer;
+        }
+
+        private string Get(int index)
+        {
+            if (index > Length) throw new IndexOutOfRangeException();
+
+            var pointer = _pmULongArray[index];
+            var pmFile = Path.Combine(PmGlobalConfiguration.PmInternalsFolder, pointer.ToString());
+            var pm = PmFactory.CreatePm(
+                new PmMemoryMappedFileConfig(
+                    pmFile));
+
+            var cSharpDefinedPm = new PmCSharpDefinedTypes(pm);
+
+            return cSharpDefinedPm.ReadString();
+        }
+    }
+}
