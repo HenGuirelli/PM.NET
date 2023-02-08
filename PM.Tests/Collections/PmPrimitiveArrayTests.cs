d744b49b8538fad0a48c43ebfa9198f0de8b1b36
Guirelli <henrique.guirelli@intelitrader.com.br>
2023-02-07 21:21:51 -0300

    Create primitive array

---
+++ PM.Tests/Collections/PmPrimitiveArrayTests.cs
@@ -0,0 +1,42 @@
+﻿using PM.Collections;
+using PM.Configs;
+using PM.Tests.Common;
+using System;
+using Xunit;
+
+namespace PM.Tests.Collections
+{
+    public class PmPrimitiveArrayTests
+    {
+        public PmPrimitiveArrayTests()
+        {
+            if (Constraints.UseFakePm)
+                PmGlobalConfiguration.PmTarget = PmTargets.InVolatileMemory;
+        }
+
+        [Fact]
+        public void OnSetAndGet_ShouldRunWithoutException()
+        {
+            var array = PmPrimitiveArray.CreateNewArray<ulong>(
+                nameof(OnSetAndGet_ShouldRunWithoutException),
+                length: 2);
+
+            array[0] = ulong.MaxValue;
+            array[1] = ulong.MinValue;
+
+            Assert.Equal(ulong.MaxValue, array[0]);
+            Assert.Equal(ulong.MinValue, array[1]);
+        }
+
+        [Fact]
+        public void OnSetAndGetOutOfBounds_ShouldThrowException()
+        {
+            var array = PmPrimitiveArray.CreateNewArray<ulong>(
+                nameof(OnSetAndGetOutOfBounds_ShouldThrowException),
+                length: 1);
+
+            Assert.Throws<IndexOutOfRangeException>(() => array[1] = ulong.MaxValue);
+            Assert.Throws<IndexOutOfRangeException>(() => array[1]);
+        }
+    }
+}
---
+++ PM/Collections/PmPrimitiveArray.cs
@@ -0,0 +1,68 @@
+﻿using PM.Core;
+using PM.Factories;
+
+namespace PM.Collections
+{
+    public abstract class PmPrimitiveArray<T>
+        where T : struct
+    {
+        protected readonly PmCSharpDefinedTypes _cSharpDefinedPm;
+        public int Length { get; private set; }
+
+        public T this[int index]
+        {
+            get => Get(index);
+            set => Set(index, value);
+        }
+
+        protected PmPrimitiveArray(string filepath, int capacity)
+        {
+            Length = capacity;
+            var pm = PmFactory.CreatePm(new PmMemoryMappedFileConfig(filepath, sizeof(ulong) * capacity));
+            _cSharpDefinedPm = new PmCSharpDefinedTypes(pm);
+        }
+
+        private void Set(int index, T value)
+        {
+            if (index > Length) throw new IndexOutOfRangeException();
+            InternalSet(index, value);
+        }
+
+        private T Get(int index)
+        {
+            if (index > Length) throw new IndexOutOfRangeException();
+            return InternalGet(index);
+        }
+
+        protected abstract void InternalSet(int index, T value);
+        protected abstract T InternalGet(int index);
+
+    }
+
+    public abstract class PmPrimitiveArray
+    {
+        public static PmPrimitiveArray<T> CreateNewArray<T>(string filepath, int length)
+            where T : struct
+        {
+            var type = typeof(T);
+            CheckType(type);
+
+            if (type == typeof(ulong))
+            {
+                var obj = new PmULongArray(filepath, length);
+                return obj as PmPrimitiveArray<T> ?? throw new ArgumentException($"Type {typeof(T)} not recongnized");
+            }
+
+            throw new ArgumentException($"Type {typeof(T)} not recongnized");
+        }
+
+        private static void CheckType(Type type)
+        {
+            if (!type.IsPrimitive &&
+                type != typeof(decimal))
+            {
+                throw new ArgumentException($"Type T must be primitive");
+            }
+        }
+    }
+}
