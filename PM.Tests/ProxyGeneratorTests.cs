ff89864b84806120c2cc71c5431de32192c88fd8
Guirelli <henrique.guirelli@intelitrader.com.br>
2023-04-12 21:38:08 -0300

    Add unti test to caching proxies

---
+++ PM.Tests/ProxyGeneratorTests.cs
@@ -0,0 +1,63 @@
+﻿using Castle.DynamicProxy;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PM.Tests
+{
+    public class DomainObj
+    {
+
+    }
+
+    public class ProxyGeneratorTests
+    {
+        [Fact]
+        public void OnCreateClassProxy_ShouldCreateObject()
+        {
+            PmProxyGenerator proxyGenerator = new();
+            var obj = proxyGenerator.CreateClassProxy(
+                typeof(DomainObj),
+                Mock.Of<IInterceptor>());
+
+            Assert.NotNull(obj);
+        }
+
+        [Fact]
+        public void OnCreateClassProxy_ShouldCreateCache()
+        {
+            PmProxyGenerator proxyGenerator = new();
+            var obj = proxyGenerator.CreateClassProxy(
+                typeof(DomainObj),
+                Mock.Of<IInterceptor>());
+
+            Thread.Sleep(200);
+
+            Assert.Equal(
+                proxyGenerator.ProxyCacheCount,
+                proxyGenerator.GetCacheCount(typeof(DomainObj)));
+        }
+
+        [Fact]
+        public void OnCreateClassProxy_WhenUseAllCache_ShouldCreateMoreCache()
+        {
+            PmProxyGenerator proxyGenerator = new(100);
+
+            for (int i = 0; i < proxyGenerator.ProxyCacheCount + 2; i++)
+            {
+                var obj = proxyGenerator.CreateClassProxy(
+                    typeof(DomainObj),
+                    Mock.Of<IInterceptor>());
+            }
+
+            Assert.Equal(
+                proxyGenerator.ProxyCacheCount,
+                proxyGenerator.GetCacheCount(typeof(DomainObj)));
+        }
+    }
+}
--- PM/ProxyGenerator.cs
+++ PM/ProxyGenerator.cs
@@ -1,18 +1,27 @@
 ﻿using Castle.DynamicProxy;
-using PM.Proxies;
 using System.Collections.Concurrent;
 
 namespace PM
 {
-    internal class PmProxyGenerator
+    public class PmProxyGenerator
     {
         private static readonly ProxyGenerator _generator = new();
         private readonly ConcurrentDictionary<Type, ConcurrentQueue<object>> _proxyCaching = new();
         private readonly ConcurrentDictionary<Type, Task> _creationProxyTasks = new();
         private readonly ConcurrentDictionary<Type, object> _creationProxyTaskLocks = new();
-        public int ProxyCacheCount { get; set; } = 100;
+        public int ProxyCacheCount { get; }
 
-        public object CreateClassProxy(Type type, PersistentInterceptor persistentInterceptor)
+        public PmProxyGenerator(int proxyCacheCount = 100)
+        {
+            ProxyCacheCount = proxyCacheCount;
+        }
+
+        public int GetCacheCount(Type type)
+        {
+            return _proxyCaching[type]?.Count ?? 0;
+        }
+
+        public object CreateClassProxy(Type type, IInterceptor interceptor)
         {
             if (_proxyCaching.TryGetValue(type, out var foundCachingItens) &&
                 foundCachingItens.TryDequeue(out var cache))
@@ -20,36 +29,51 @@ namespace PM
                 return cache;
             }
 
-            StartPopulateCache(type, persistentInterceptor);
+            StartPopulateCache(type, interceptor);
 
-            return _generator.CreateClassProxy(type, persistentInterceptor);
+            return CreateClassProxy(type, interceptor);
         }
 
         private void StartPopulateCache(
             Type type,
-            PersistentInterceptor persistentInterceptor)
+            IInterceptor interceptor)
         {
             lock (_creationProxyTaskLocks.GetOrAdd(type, new object()))
             {
-                if (_proxyCaching.TryGetValue(type, out var cachingItens))
+                if (_proxyCaching.TryGetValue(type, out var cachingItens) &&
+                    _creationProxyTasks.TryGetValue(type, out var task))
                 {
-                    if (!cachingItens.IsEmpty)
+                    if (task.IsCompleted && cachingItens.IsEmpty)
                     {
+                        Task t1 = StartNewTask(type, interceptor);
+                        _creationProxyTasks[type] = t1;
                         return;
                     }
+                    return;
                 }
 
-                var notFoundCachingItens = _proxyCaching[type] = new ConcurrentQueue<object>();
-                var t = Task.Run(() =>
-                {
-                    for (int i = 0; i < ProxyCacheCount; i++)
-                    {
-                        notFoundCachingItens.Enqueue(
-                            _generator.CreateClassProxy(type, persistentInterceptor));
-                    }
-                });
-                _creationProxyTasks[type] = t;
+                Task t2 = StartNewTask(type, interceptor);
+                _creationProxyTasks[type] = t2;
             }
+        }
+
+        private Task StartNewTask(Type type, IInterceptor interceptor)
+        {
+            var notFoundCachingItens = _proxyCaching[type] = new ConcurrentQueue<object>();
+            var firstItemQueued = false;
+            var t = Task.Run(() =>
+            {
+                // ProxyCacheCount + 1 because the first object is
+                // immediatly dequeued to return to caller.
+                for (int i = 0; i < ProxyCacheCount + 1; i++)
+                {
+                    notFoundCachingItens.Enqueue(
+                        _generator.CreateClassProxy(type, interceptor));
+                    firstItemQueued = true;
+                }
+            });
+            SpinWait.SpinUntil(() => firstItemQueued);
+            return t;
         }
     }
 }
