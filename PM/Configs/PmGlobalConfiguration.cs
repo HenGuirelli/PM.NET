b5e12de678c5fb6292d8444cf84df0dfe7caf86f
HenGuirelli <hen.guirelli@gmail.com>
2023-07-26 13:14:27 -0300

    Add automatically directory creation

--- PM/Configs/PmGlobalConfiguration.cs
+++ PM/Configs/PmGlobalConfiguration.cs
@@ -6,8 +6,58 @@ namespace PM.Configs
     {
         public static PmTargets PmTarget { get; set; } = PmTargets.PM;
         public static int CollectFileInterval { get; set; } = 120000;
-        public static string PmInternalsFolder { get; set; } = Path.Combine("pm", "internals");
-        public static string PmTransactionFolder => Path.Combine(PmInternalsFolder, "transactions");
+
+        #region PmInternalsFolder
+        private static string _pmInternalsFolder = Path.Combine("pm", "internals");
+        private static bool _pmInternalsFolderCreated = false;
+        public static string PmInternalsFolder
+        {
+            get
+            {
+                if (!_pmInternalsFolderCreated && !Directory.Exists(_pmInternalsFolder))
+                {
+                    Directory.CreateDirectory(_pmInternalsFolder);
+                    _pmInternalsFolderCreated = true;
+                }
+                return _pmInternalsFolder;
+            }
+            set
+            {
+                if (!_pmInternalsFolderCreated && !Directory.Exists(_pmInternalsFolder))
+                {
+                    Directory.CreateDirectory(_pmInternalsFolder);
+                    _pmInternalsFolderCreated = true;
+                }
+                _pmInternalsFolder = value;
+            }
+        }
+        #endregion PmInternalsFolder
+
+        #region PmTransactionFolder
+        private static string _pmTransactionFolder = Path.Combine(PmInternalsFolder, "transactions");
+        private static bool _pmTransactionFolderCreated = false;
+        public static string PmTransactionFolder
+        {
+            get
+            {
+                if (!_pmTransactionFolderCreated && !Directory.Exists(_pmTransactionFolder))
+                {
+                    Directory.CreateDirectory(_pmTransactionFolder);
+                    _pmTransactionFolderCreated = true;
+                }
+                return _pmTransactionFolder;
+            }
+            set
+            {
+                if (!_pmTransactionFolderCreated && !Directory.Exists(_pmTransactionFolder))
+                {
+                    Directory.CreateDirectory(_pmTransactionFolder);
+                    _pmTransactionFolderCreated = true;
+                }
+                _pmTransactionFolder = value;
+            }
+        }
+        #endregion PmTransactionFolder
 
         public static PmLogger Logger { get; set; } = new PmLogger();
     }
