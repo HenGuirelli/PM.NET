4ed1eeb3644952e0c7148862d252568e43320550
guirelli <hen.guirelli@gmail.com>
2023-12-02 19:10:22 -0300

    Create PmMemCpyStream unit tests

---
+++ PM.Core.Tests/PmMarshalStreamTests.cs
@@ -0,0 +1,28 @@
+﻿using PM.Tests.Common;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace PM.Core.Tests
+{
+    public class PmMarshalStreamTests : UnitTest
+    {
+        [Fact]
+        public void OnWriteAndReadOnPmMarshalStream()
+        {
+            using var stream = new PmMarshalStream(
+                CreateFilePath(nameof(OnWriteAndReadOnPmMarshalStream)),
+                4096);
+
+            var bytesToWrite = Encoding.UTF8.GetBytes("Hello PM!");
+            stream.Write(bytesToWrite);
+            stream.Flush();
+
+            var buffer = new byte[bytesToWrite.Length];
+            stream.Seek(0, System.IO.SeekOrigin.Begin);
+            stream.Read(buffer, 0, buffer.Length);
+
+            Assert.Equal("Hello PM!", Encoding.UTF8.GetString(buffer));
+        }
+    }
+}
