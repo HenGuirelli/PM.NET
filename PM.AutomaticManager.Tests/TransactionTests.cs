6ea799286e3447fb583f212b58157e1e65928045
guirelli <hen.guirelli@gmail.com>
2024-05-22 23:37:04 -0300

    Add test to garantee transaction isolation

--- PM.AutomaticManager.Tests/TransactionTests.cs
+++ PM.AutomaticManager.Tests/TransactionTests.cs
@@ -64,5 +64,74 @@ namespace PM.AutomaticManager.Tests
             var decoded = PMemoryDecoder.DecodeHex(factory.Allocator.ReadOriginalFile(), dump: false);
             _output.WriteLine(decoded);
         }
+
+        /// <summary>
+        /// fazer com theads e garantir isolamento
+        /// </summary>
+        [Fact]
+        public void OnTransaction_TrasactionValuesShouldRunOnlyInsideTransaction()
+        {
+#if DEBUG
+            PersistentFactory.Purge();
+#endif
+
+            PmGlobalConfiguration.PmTarget = Core.PmTargets.TraditionalMemoryMappedFile;
+
+            var factory = new PersistentFactory();
+            var proxyObj = factory.CreateRootObject<RootClass>(nameof(OnTransaction_WithMultipleObjects_ShouldDoTransaction));
+
+            bool error = false;
+
+            var tTransaction = new Thread(() =>
+            {
+                try
+                {
+                    proxyObj.Transaction(factory.PMemoryManager, () =>
+                    {
+                        proxyObj.InnerObject1 = new InnerClass1 { Val = int.MaxValue };
+                        proxyObj.InnerObject2 = new InnerClass1 { Val = int.MinValue };
+                        proxyObj.InnerObject1.InnerObject1 = new InnerClass2 { Val = int.MinValue };
+                        proxyObj.Val = 10000;
+                        Thread.Sleep(500);
+                    });
+
+                    Assert.Equal(int.MaxValue, proxyObj.InnerObject1.Val);
+                    Assert.Equal(int.MinValue, proxyObj.InnerObject2.Val);
+                    Assert.Equal(int.MinValue, proxyObj.InnerObject1.InnerObject1.Val);
+                    Assert.Equal(10000, proxyObj.Val);
+                }
+                catch (Exception ex)
+                {
+                    error = true;
+                }
+            });
+
+            var tValidation = new Thread(() =>
+            {
+                try
+                {
+                    // Wait tTransaction starts
+                    Thread.Sleep(100);
+
+                    Assert.Null(proxyObj.InnerObject1);
+                    Assert.Null(proxyObj.InnerObject2);
+                }
+                catch (Exception ex)
+                {
+                    error = true;
+                }
+            });
+
+            tTransaction.Start();
+            tValidation.Start();
+
+            tTransaction.Join();
+            tValidation.Join();
+
+            var decoded = PMemoryDecoder.DecodeHex(factory.Allocator.ReadOriginalFile(), dump: false);
+            _output.WriteLine(decoded);
+
+            Assert.False(error);
+        }
     }
 }
