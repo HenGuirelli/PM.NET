1c43099d6e4cb082303cfed062374cd62c03e42b
guirelli <hen.guirelli@gmail.com>
2024-06-03 19:47:35 -0300

    Add profiling project to run and make a profiling (ex: using perfview)

---
+++ ProfilingTests/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using PM.AutomaticManager;
+using PM.AutomaticManager.Configs;
+using ProfilingTests;
+
+const string ValueToWrite = "TextValue";
+
+PmGlobalConfiguration.PmTarget = PM.Core.PmTargets.TraditionalMemoryMappedFile;
+PmGlobalConfiguration.PmInternalsFolder = "./ProflingTests";
+
+var _persistentFactorySSD = new PersistentFactory();
+var _proxy = _persistentFactorySSD.CreateRootObject<RootObject>("RootObj");
+
+
+while (true)
+{
+    _proxy.StringVal = ValueToWrite;
+}
---
+++ ProfilingTests/RootObject.cs
@@ -0,0 +1,25 @@
+﻿namespace ProfilingTests
+{
+    public class RootObject
+    {
+        public virtual int IntVal { get; set; }
+
+        public virtual long LongVal { get; set; }
+
+        public virtual short ShortVal { get; set; }
+
+        public virtual byte ByteVal { get; set; }
+
+        public virtual double DoubleVal { get; set; }
+
+        public virtual float FloatVal { get; set; }
+
+        public virtual decimal DecimalVal { get; set; }
+
+        public virtual char CharVal { get; set; }
+
+        public virtual bool BoolVal { get; set; }
+
+        public virtual string StringVal { get; set; }
+    }
+}
